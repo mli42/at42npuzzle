25fc09bdb03fbbd5cdb6f3f86b202bbf8483ba3b
mli42 <mli@student.42.fr>
2022-04-15 14:50:07 +0200

    Size 3 per default

--- srcs/Node.cpp
+++ srcs/Node.cpp
@@ -1,7 +1,7 @@
 #include "../includes/Node.hpp"
 
 std::string Node::heuristic_type;
-size_t Node::size = 4;
+size_t Node::size = 3;
 size_t Node::double_size = Node::size * Node::size;
 bool Node::unsolvable = false;
 bool Node::visualizer = false;
